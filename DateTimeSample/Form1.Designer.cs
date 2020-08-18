e7b0a69b24f3c923d06a9a126ba83eed7eb9b71b
YukiEmori <31040@ojs.ac.jp>
2020-08-18 14:54:23 +0900

    閏年判定及び日付計算ボタンの追加

--- DateTimeSample/Form1.Designer.cs
+++ DateTimeSample/Form1.Designer.cs
@@ -72,7 +72,7 @@
             // tbNowDisp
             // 
             this.tbNowDisp.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.tbNowDisp.Location = new System.Drawing.Point(197, 35);
+            this.tbNowDisp.Location = new System.Drawing.Point(198, 35);
             this.tbNowDisp.Name = "tbNowDisp";
             this.tbNowDisp.Size = new System.Drawing.Size(422, 39);
             this.tbNowDisp.TabIndex = 1;
